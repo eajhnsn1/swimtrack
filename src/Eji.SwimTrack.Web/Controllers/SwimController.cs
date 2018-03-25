5bff8d77da603e2fb3a171e814644b2a0375030e
Eric Johnson <ej@ejinnovations.com>
2018-03-25 12:27:18 -0500

    swim controller added

---
+++ src/Eji.SwimTrack.Web/Controllers/SwimController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eji.SwimTrack.Service.Models;
+using Eji.SwimTrack.Web.ServiceClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eji.SwimTrack.Web.Controllers
+{
+    public class SwimController : Controller
+    {
+        ISwimServiceClient swimService = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SwimController(ISwimServiceClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            swimService = client;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<SwimData> data = await swimService.GetAllSwimsAsync();
+
+            // temporary just for the grid
+            ViewBag.Data = data;
+
+            return View();
+        }
+    }
+}

1fbf33aa44e12bcb8ab9abbc6ef13e28850c9b59
Eric Johnson <ej@ejinnovations.com>
2018-03-31 10:43:09 -0500

    starting swim viewmodels

---
+++ src/Eji.SwimTrack.Web/Models/SwimViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Eji.SwimTrack.Service.Models;
+using Eji.SwimTrack.Service.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eji.SwimTrack.Web.Models
+{
+    /// <summary>
+    /// Single swim viwmodel
+    /// </summary>
+    public class SwimViewModel
+    {
+        SwimData swim = null;
+
+        public string Distance
+        {
+            get
+            {
+                return $"{swim.Distance} { GetDistanceName(swim.DistanceUnits)}";
+            }
+        }
+
+        private string GetDistanceName(DistanceUnits units)
+        {
+            switch (units)
+            {
+                case DistanceUnits.Meters:
+                    return "meter";
+                case DistanceUnits.Yards:
+                    return "yard";
+                default:
+                    return "";
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SwimViewModel(SwimData swim)
+        {
+            if (swim == null)
+            {
+                throw new ArgumentNullException(nameof(swim));
+            }
+
+            this.swim = swim;
+        }
+    }
+}

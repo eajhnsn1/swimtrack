56f1ca05f74da0cf661611dbc18ddc15f841c267
Eric Johnson <ej@ejinnovations.com>
2018-04-08 12:31:00 -0500

    SwimTime.FromSeconds

---
+++ src/Eji.SwimTrack.Web.Tests/SwimTimeTests/FromSecondsShould.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Eji.SwimTrack.Web.Utility;
+
+namespace Eji.SwimTrack.Web.Tests.SwimTimeTests
+{
+    public class FromSecondsShould
+    {
+        [Fact]
+        public void Throw_GivenNegativeSeconds()
+        {
+            Action act = () => SwimTime.FromSeconds(-1);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ReturnAMinute_Given60Seconds()
+        {
+            SwimTime time = SwimTime.FromSeconds(60);
+            time.Minutes.Should().Be(1);
+            time.Seconds.Should().Be(0);
+            time.Hundredths.Should().Be(0);
+        }
+
+        [Fact]
+        public void ReturnMinutes_GivenMoreThan60Seconds()
+        {
+            SwimTime time = SwimTime.FromSeconds(120);
+            time.Minutes.Should().Be(2);
+            time.Seconds.Should().Be(0);
+            time.Hundredths.Should().Be(0);
+        }
+
+        [Fact]
+        public void ReturnMinutesAndSeconds_GivenMoreThan60Seconds()
+        {
+            SwimTime time = SwimTime.FromSeconds(130);
+            time.Minutes.Should().Be(2);
+            time.Seconds.Should().Be(10);
+            time.Hundredths.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void ReturnHundredths()
+        {
+            SwimTime time = SwimTime.FromSeconds(1.02M);
+            time.Minutes.Should().Be(0);
+            time.Seconds.Should().Be(1);
+            time.Hundredths.Should().Be(2);
+        }
+
+    }
+}
---
+++ src/Eji.SwimTrack.Web/Utility/SwimTime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eji.SwimTrack.Web.Utility
+{
+    /// <summary>
+    /// Represents a swim time result
+    /// </summary>
+    public class SwimTime
+    {
+        decimal? totalSeconds;
+
+        public int Minutes
+        {
+            get; internal set;
+        }
+        public int Seconds
+        {
+            get; internal set;
+        }
+        public int Hundredths
+        {
+            get; internal set;
+        }
+
+        public static SwimTime FromSeconds(decimal seconds)
+        {
+            if (seconds < 0)
+            {
+                throw new ArgumentException($"{ nameof(seconds) } must be greater than or equal to zero.", nameof(seconds));
+            }
+
+
+            int min = (int)seconds / 60;
+            int sec = (int)seconds % 60;
+            int hundredths = (int)((seconds - Math.Floor(seconds)) * 100);
+
+            return new SwimTime()
+            {
+                Minutes = min,
+                Seconds = sec,
+                Hundredths = hundredths
+            };
+        }
+    }
+}
